32df50055d80ed902040a945c83c731fc520485c
Marciusz66 <marciistvan45@outlook.hu>
2021-11-26 15:59:47 +0100

    added curri-b.sql

---
+++ curri-b.sql
@@ -0,0 +1,19 @@
+SELECT * FROM projects
+WHERE project_name = 'Introduction to SQL';
+
+SELECT * FROM projects
+WHERE project_name = 'Setup PostgreSQL';
+
+SELECT * FROM projects
+WHERE project_name = 'Project Codewars - 7 kyu in SQL';
+
+SELECT * FROM projects
+WHERE project_name = 'TodoDB.sql';
+
+SELECT * FROM projects
+WHERE project_name = 'Northwind Traders';
+
+SELECT * FROM projects
+WHERE project_name = 'Curriculum';
+
+SELECT * FROM projects;
